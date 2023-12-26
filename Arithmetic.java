2b04d29ee066553f139dcfd4acc4aa452c1ec3f7
Rashithselva <ravirashith@gmail.com>
2023-12-26 20:28:51 +0530

    Throw Exception

--- Arithmetic.java
+++ Arithmetic.java
@@ -3,13 +3,11 @@ package Exception;
 public class Arithmetic {
      public static void main(String[] args) {
         
-        int a=0;
-        try{
-                    System.out.println(10/a);
+        System.out.println("Planed start");
 
-        }catch(ArithmeticException e){
-            System.out.println("It's worng give the correct number");
-        }
+        System.out.println(2);
+        System.out.println(10/0);
+        
     }
     
 }
