99f589fa44d7d7c9e3ea0679c5fe7354d99e43c6
Rashithselva <ravirashith@gmail.com>
2023-12-26 20:16:32 +0530

    Exception handled

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,7 +1,15 @@
 package Exception;
 
 public class Arithmetic {
-   
+     public static void main(String[] args) {
+        
+        int a=0;
+        try{
+                    System.out.println(10/a);
 
+        }catch(ArithmeticException e){
+            System.out.println("It's worng give the correct number");
+        }
+    }
     
 }
