3b809cfd401bf65a2245e27c86e4f1b85c6fea5e
Rashithselva <ravirashith@gmail.com>
2023-12-27 20:53:11 +0530

    Throw the coustom exception

--- Customex.java
+++ Customex.java
@@ -1,5 +1,20 @@
 package Exception;
+import java.util.Scanner;
 
 public class Customex {
+    public static void main(String[] args) {
+        Scanner r=new Scanner(System.in);
+
+        System.out.println("Enter your age");
+        int age=r.nextInt();
+
+        if(age<18){
+            try{
+                throw new NoteligibleException ();
+            }catch(NoteligibleException e){
+                System.out.println("your age is below 18 ,you are not eligible");
+            }
+        }
+    }
     
 }
---
+++ NoteligibleException.java
@@ -0,0 +1,5 @@
+package Exception;
+
+public class NoteligibleException extends Exception{
+    
+}
